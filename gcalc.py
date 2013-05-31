06b1a0d48e992f95890ce9a2ab93b6020cee8439
Matthew Hielscher <mhielscher@gmail.com>
2013-05-30 22:33:11 -0700

    Converted to use requests and unicode, fixed fractional answers.

--- gcalc.py
+++ gcalc.py
@@ -16,11 +16,15 @@ import os
 import readline
 import atexit
 import sys
-import urllib
-import urllib2
+import requests
 import simplejson as json
+import re
 
-error_codes = {
+__VERSION__ = "2013.5.30"
+
+USER_AGENT = "gcalc/"+__VERSION__+" "+requests.utils.default_user_agent()
+
+ERROR_CODES = {
     "0": "No calculation identified.",
     "4": "Did not recognize input.",
     }
@@ -29,90 +33,99 @@ def process_query(q):
 	"""Send the query, interpret the response, and return a composed string."""
 	# Encode query, make request, read response
 	url = "http://www.google.com/ig/calculator"
-	data = [("hl", "en"), ("q", q)]
-	postdata = urllib.urlencode(data)
-	url = url+'?'+postdata
-	f = urllib2.urlopen(url)
-	r = f.read()
+	data = {"hl": "en", "q": q}
+	r = requests.get(url, params=data, headers={'User-Agent': USER_AGENT})
+	if r.status_code != 200:
+		return "Error: HTTP status %d" % r.status_code
+	
+	s = r.text
 	# Apparently this is the encoding of the return?
-	r = r.decode('cp1252')
+	#s = s.decode('cp1252')
 
 	# Correct the invalid JSON
-	r = r.replace('{', '{"')
-	r = r.replace(': ', '":')
-	r = r.replace(',', ', "')
-	r = r.replace(u'\\x', u'\\u00')
+	s = s.replace('{', '{"')
+	s = s.replace(': ', '":')
+	s = s.replace(',', ', "')
+	s = s.replace(u'\\x', u'\\u00')
 	# Replace the nonbreaking space that python chokes on
-	r = r.replace(u'\xa0', u',')
+	s = s.replace(u'\xa0', u',')
 	# Replace obfuscated escape sequence with the proper character
 	if os.environ['LANG'].lower().find('utf'):
-	    r = r.replace(u'\\u0026#215;', u'×')
+	    s = s.replace(u'\\u0026#215;', u'×')
 	else:
-	    r = r.replace(u'\\u0026#215;', u'x')
+	    s = s.replace(u'\\u0026#215;', u'x')
 
 	# Parse JSON
-	jdata = json.loads(r)
+	try:
+		jdata = json.loads(s)
+	except Exception as e:
+		print s
+		raise e
 
 	#print result
 	if jdata['error'] != "":
 		error = jdata['error']
-		if error in error_codes.keys():
-		    error = error_codes[error]
+		if error in ERROR_CODES.keys():
+		    error = ERROR_CODES[error]
 		return "Error: "+error
 	else:
 		lhs = jdata['lhs']
 		rhs = jdata['rhs']
-		sup_start = rhs.find("<sup>")
-		if sup_start != -1:
-			sup_end = rhs.find("</sup>")
-			rhs = rhs[:sup_start] + "^" + rhs[sup_start+5:sup_end] + rhs[sup_end+6:]
+		rhs = re.sub(r'<sup>(.*)</sup>&#8260;<sub>(.*)</sub>', r' \1/\2', rhs)
+		rhs = re.sub(r'<sup>(.*)</sup>', r'^\1', rhs)
+		#sup_start = rhs.find("<sup>")
+		#if sup_start != -1:
+		#	sup_end = rhs.find("</sup>")
+		#	rhs = rhs[:sup_start] + "^" + rhs[sup_start+5:sup_end] + rhs[sup_end+6:]
 		return lhs + " = " + rhs
 
-interactive = False
-initialquery = None
-is_shell = sys.stdin.isatty()
 
-# Read options, initial query, interactive mode
-if "-h" in sys.argv:
-	print __doc__
-	exit(0)
-elif len(sys.argv) < 2:
-	interactive = True
-elif sys.argv[1] == "-i":
-	interactive = True
-	if len(sys.argv) > 2:
-		initialquery = sys.argv[2]
-else:
-	initialquery = sys.argv[1]
+if __name__ == '__main__':
+    interactive = False
+    initialquery = None
+    is_shell = sys.stdin.isatty()
 
-# Process query entered on the command line if it exists
-if initialquery:
-	print process_query(initialquery)
+    # Read options, initial query, interactive mode
+    if "-h" in sys.argv:
+	    print __doc__
+	    exit(0)
+    elif len(sys.argv) < 2:
+	    interactive = True
+    elif sys.argv[1] == "-i":
+	    interactive = True
+	    if len(sys.argv) > 2:
+		    initialquery = sys.argv[2]
+    else:
+	    initialquery = sys.argv[1]
 
-# Register the history file for the interactive shell
-if interactive and is_shell:
-	def save_history(histfile):
-		readline.write_history_file(histfile)
+    # Process query entered on the command line if it exists
+    if initialquery:
+	    print process_query(initialquery)
 
-	histfile = os.path.join(os.environ["HOME"], ".gcalc_history")
-	try:
-		readline.read_history_file(histfile)
-	except IOError:
-		pass
+    # Register the history file for the interactive shell
+    if interactive and is_shell:
+	    def save_history(histfile):
+		    readline.write_history_file(histfile)
 
-	atexit.register(save_history, histfile)
+	    histfile = os.path.join(os.environ["HOME"], ".gcalc_history")
+	    try:
+		    readline.read_history_file(histfile)
+	    except IOError:
+		    pass
 
-# Enter the interactive shell - end with 'quit' or 'exit'
-while interactive:
-	try:
-		if is_shell:
-			inputline = raw_input("; ")
-		else:
-			inputline = sys.stdin.readline()
-	except EOFError:
-		if is_shell: print ''
-		sys.exit(0)
-	if inputline == "exit" or inputline == "quit" or (not is_shell and inputline == ''):
-		sys.exit(0)
-	print process_query(inputline.strip())
+	    atexit.register(save_history, histfile)
+
+    # Enter the interactive shell - end with 'quit' or 'exit'
+    while interactive:
+	    try:
+		    if is_shell:
+			    inputline = raw_input("; ")
+		    else:
+			    inputline = sys.stdin.readline()
+	    except EOFError:
+		    if is_shell: print ''
+		    sys.exit(0)
+	    if inputline == "exit" or inputline == "quit" or (not is_shell and inputline == ''):
+		    sys.exit(0)
+	    print process_query(inputline.strip())
 
