cfb8c842c28c45f4ab5c3604c29db0f4d62f28b1
Matthew Hielscher <mhielscher@gmail.com>
2013-11-15 01:54:38 -0800

    Replaced defunct iGoogle API with web search scraping.

--- gcalc.py
+++ gcalc.py
@@ -19,10 +19,12 @@ import sys
 import requests
 import simplejson as json
 import re
+from bs4 import BeautifulSoup
 
 __VERSION__ = "2013.8.25"
 
-USER_AGENT = "gcalc/"+__VERSION__+" "+requests.utils.default_user_agent()
+#USER_AGENT = "gcalc/"+__VERSION__+" "+requests.utils.default_user_agent()
+USER_AGENT = "gcalc/"+__VERSION__+" "+"Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36"
 
 ERROR_CODES = {
     "0": "No calculation identified.",
@@ -30,52 +32,38 @@ ERROR_CODES = {
     }
 
 def process_query(q):
-	"""Send the query, interpret the response, and return a composed string."""
-	# Encode query, make request, read response
-	url = "http://www.google.com/ig/calculator"
-	data = {"hl": "en", "q": q}
-	r = requests.get(url, params=data, headers={'User-Agent': USER_AGENT})
-	if r.status_code != 200:
-		return "Error: HTTP status %d" % r.status_code
-	
-	s = r.text
-	# Apparently this is the encoding of the return?
-	#s = s.decode('cp1252')
+    """Send the query, interpret the response, and return a composed string."""
+    # Encode query, make request, read response
+    # The iGoogle JSON-like interface is gone, so resort to screen-scraping.
+    url = "http://www.google.com/search"
+    data = {"hl": "en", "q": q}
+    r = requests.get(url, params=data, headers={'User-Agent': USER_AGENT})
+    if r.status_code != 200:
+        return "Error: HTTP status %d" % r.status_code
+    
+    s = r.text
+    
+    # Replace obfuscated escape sequence with the proper character
+    if os.environ['LANG'].lower().find('utf') != -1:
+        s = s.replace(u'\\u0026#215;', u'×')
+    else:
+        s = s.replace(u'\\u0026#215;', u'x')
 
-	# Correct the invalid JSON
-	s = s.replace('{', '{"')
-	s = s.replace(': ', '":')
-	s = s.replace(',', ', "')
-	s = s.replace(u'\\x', u'\\u00')
-	# Replace the nonbreaking space that python chokes on
-	s = s.replace(u'\xa0', u',')
-	# Replace obfuscated escape sequence with the proper character
-	if os.environ['LANG'].lower().find('utf') != -1:
-	    s = s.replace(u'\\u0026#215;', u'×')
-	else:
-	    s = s.replace(u'\\u0026#215;', u'x')
-
-	# Parse JSON
-	try:
-		jdata = json.loads(s)
-	except Exception as e:
-		print s
-		raise e
-
-	#print result
-	if jdata['error'] != "":
-		error = jdata['error']
-		if error in ERROR_CODES.keys():
-		    error = ERROR_CODES[error]
-		return "Error: "+error
-	else:
-		lhs = jdata['lhs']
-		rhs = jdata['rhs']
-		# Translate fractions
-		rhs = re.sub(r'<sup>(.*)</sup>&#8260;<sub>(.*)</sub>', r' \1/\2', rhs)
-		# Translate exponents
-		rhs = re.sub(r'<sup>(.*)</sup>', r'^\1', rhs)
-		return lhs + " = " + rhs
+    page = BeautifulSoup(s)
+    if page.find("div", "vk_ans") != None:
+        lhs = ''.join(unicode(t).strip() for t in page.find("div", "vk_gy").contents)
+        rhs = ''.join(unicode(t).strip() for t in page.find("div", "vk_ans").contents)
+    elif page.find("span", "cwcot") != None:
+        lhs = ''.join(unicode(t).strip() for t in page.find("span", "cwclet").contents)
+        rhs = ''.join(unicode(t).strip() for t in page.find("span", "cwcot").contents)
+    else:
+        return "Error: Expression not recognized."
+    
+    # Translate fractions
+    rhs = re.sub(r'<sup>(.*)</sup>&#8260;<sub>(.*)</sub>', r' \1/\2', rhs)
+    # Translate exponents
+    rhs = re.sub(r'<sup>(.*)</sup>', r'^\1', rhs)
+    return lhs + " " + rhs
 
 
 if __name__ == '__main__':
@@ -85,45 +73,45 @@ if __name__ == '__main__':
 
     # Read options, initial query, interactive mode
     if "-h" in sys.argv:
-	    print __doc__
-	    exit(0)
+        print __doc__
+        exit(0)
     elif len(sys.argv) < 2:
-	    interactive = True
+        interactive = True
     elif sys.argv[1] == "-i":
-	    interactive = True
-	    if len(sys.argv) > 2:
-		    initialquery = sys.argv[2]
+        interactive = True
+        if len(sys.argv) > 2:
+            initialquery = sys.argv[2]
     else:
-	    initialquery = sys.argv[1]
+        initialquery = sys.argv[1]
 
     # Process query entered on the command line if it exists
     if initialquery:
-	    print process_query(initialquery)
+        print process_query(initialquery)
 
     # Register the history file for the interactive shell
     if interactive and is_shell:
-	    def save_history(histfile):
-		    readline.write_history_file(histfile)
+        def save_history(histfile):
+            readline.write_history_file(histfile)
 
-	    histfile = os.path.join(os.environ["HOME"], ".gcalc_history")
-	    try:
-		    readline.read_history_file(histfile)
-	    except IOError:
-		    pass
+        histfile = os.path.join(os.environ["HOME"], ".gcalc_history")
+        try:
+            readline.read_history_file(histfile)
+        except IOError:
+            pass
 
-	    atexit.register(save_history, histfile)
+        atexit.register(save_history, histfile)
 
     # Enter the interactive shell - end with 'quit' or 'exit'
     while interactive:
-	    try:
-		    if is_shell:
-			    inputline = raw_input("; ")
-		    else:
-			    inputline = sys.stdin.readline()
-	    except EOFError:
-		    if is_shell: print ''
-		    sys.exit(0)
-	    if inputline == "exit" or inputline == "quit" or (not is_shell and inputline == ''):
-		    sys.exit(0)
-	    print process_query(inputline.strip())
+        try:
+            if is_shell:
+                inputline = raw_input("; ")
+            else:
+                inputline = sys.stdin.readline()
+        except EOFError:
+            if is_shell: print ''
+            sys.exit(0)
+        if inputline == "exit" or inputline == "quit" or (not is_shell and inputline == ''):
+            sys.exit(0)
+        print process_query(inputline.strip())
 
