af1b4f681ce8c84442d346c8deb3cb77d427ed41
Matthew Hielscher <mhielscher@gmail.com>
2018-05-20 09:11:05 -0700

    Fixed a big where elem != str(elem)

--- gcalc.py
+++ gcalc.py
@@ -19,6 +19,7 @@ import sys
 import json
 import re
 import threading
+import time
 
 
 ## Load the heavy modules in the background after the prompt comes up
@@ -82,28 +83,34 @@ def process_query(q):
     while BeautifulSoup == None: # Wait for BeautifulSoup (with lxml) to load
         time.sleep(0.1)
     page = BeautifulSoup(s, "lxml")
-    # Calculation with operators
+
+    # Most calculations:
     if page.find("div", "vk_bk") != None:
-        lhs = ''.join(t.strip() for t in page.find("div", "vk_gy").contents)
-        rhs = ''.join(t.strip() for t in page.find("div", "dDoNo").contents)
-    # ?
+        lhs = ''.join(str(t).strip() for t in page.find("div", "vk_gy").contents)
+        rhs = ''.join(str(t).strip() for t in page.find("div", "dDoNo").contents)
+
+    # ...deprecated?:
     elif page.find("span", "cwcot") != None:
-        lhs = ''.join(t.strip() for t in page.find("span", "cwclet").contents)
-        rhs = ''.join(t.strip() for t in page.find("span", "cwcot").contents)
-    # ?
+        lhs = ''.join(str(t).strip() for t in page.find("span", "cwclet").contents)
+        rhs = ''.join(str(t).strip() for t in page.find("span", "cwcot").contents)
+
+    # Older unit conversion (probably unused now):
     elif page.find("input", id="ucw_lhs_d") != None:
-        # This does not include units; not sure how to grab those
+        # This does not include unit names; not sure how to grab those
         lhs = page.find("input", id="ucw_lhs_d")['value'] + " ="
         rhs = page.find("input", id="ucw_rhs_d")['value']
-    # Unit conversion
+
+    # New unit conversion with dropdowns:
     elif page.find("div", "vk_c") != None:
         # Return first hit if it exists, else dummy tag
         values = [e['value'] for e in page.select("div.vk_c input")]
         units = [e.text for e in page.select(".vk_c select option[selected]")][1:]
         lhs = "%s %s = " % (values[0], units[0])
         rhs = "%s %s" % (values[1], units[1])
+
     else:
         return "Error: Could not find answer on result page.\nQuery: {}\nDirect link: {}".format(q, r.url)
+
     
     # Translate fractions
     rhs = re.sub(r'<sup>(.*)</sup>&#8260;<sub>(.*)</sub>', r' \1/\2', rhs)
